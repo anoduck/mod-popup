98b1ac8932085c2cd430b90ffa2367fcf9672f15
anoduck <9925396+anoduck@users.noreply.github.com>
2025-01-31 18:37:34 -0500

    style: wIP: Styling
    Not satisfied with Popup style, rewriting popup.
    
    BREAKING CHANGE: WIP: Means everything is broken.

--- contrib/js/autoload.js
+++ contrib/js/autoload.js
@@ -1,19 +1,13 @@
-() => {
-	'use strict';
-	document.addEventListener('DOMContentLoaded', () => {
-		// Check if the page is under construction
-		if (document.body.classList.contains('under-construction')) {
-			// Create the overlay element
-			const overlay = document.createElement('div');
-			overlay.id = 'under-construction-overlay';
-			overlay.innerHTML = `
-			<div class="content">
-				<h1>Under Construction</h1>
-				<p>We are currently working on this page. Please check back later.</p>
-			</div>
-			`;
-			// Append the overlay to the body
-			document.body.appendChild(overlay);
-		}
-			});
-}
+$(function() {
+    $('.pop-up').hide();
+    $('.pop-up').fadeIn(1000);
+    
+        $('.close-button').click(function (e) { 
+  
+        $('.pop-up').fadeOut(700);
+        $('#overlay').removeClass('blur-in');
+        $('#overlay').addClass('blur-out');
+        e.stopPropagation();
+          
+      });
+  });
--- exampleSite/go.mod
+++ exampleSite/go.mod
@@ -2,4 +2,4 @@ module github.com/anoduck/mod-popup/exampleSite
 
 go 1.19
 
-require github.com/anoduck/mod-popup v0.0.0-20240912031743-7fd02bc201de // indirect
+require github.com/anoduck/mod-popup v0.0.1
